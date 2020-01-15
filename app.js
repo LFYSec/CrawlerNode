24cd490550fb5305f5db03d13b0ee52b8bea170a
LFY <751502495@qq.com>
2020-01-15 18:06:16 +0800

    adjust func

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 import {Cluster} from 'puppeteer-cluster'
-import fs from 'fs'
-import request from 'request'
 import * as func from './func'
 import * as config from './config'
 
@@ -8,144 +6,13 @@ async function preparePage(page) {
     // set ua
     await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36");
     // insert hook code
-    await page.evaluateOnNewDocument(hookJS);
+    await page.evaluateOnNewDocument(func.hookJS);
     // open req intercept
     await page.setRequestInterception(true);
     // open js ability
     await page.setJavaScriptEnabled(true);
     // close catch
     await page.setCacheEnabled(false);
-}
-
-function hookJS() {
-    const unChange = {writable: false, configurable: false};
-
-    // hook history route
-    window.history.pushState = function (a, b, url) {
-        console.log(url);
-    };
-    Object.defineProperty(window.history, "pushState", unChange);
-
-    window.history.replaceState = function (a, b, url) {
-        console.log(url);
-    };
-    Object.defineProperty(window.history, "replaceState", unChange);
-
-    // hook hash change route
-    window.addEventListener("hashchange", function () {
-        console.log(document.location.href);
-    });
-
-    // hook new window
-    window.open = function (url) {
-        console.log(url);
-    };
-    Object.defineProperty(window, 'open', unChange);
-
-    window.close = function () {
-        console.log(`trying to close page.`);
-    };
-    Object.defineProperty(window, "close", unChange);
-
-    // hook new requests
-    let oldWebSocket = window.WebSocket;
-    window.WebSocket = function (url) {
-        console.log(`WebSocket: ${url}`);
-        return new oldWebSocket(url);
-    };
-
-    let oldEventSource = window.EventSource;
-    window.EventSource = function (url) {
-        console.log(`EventSource: ${url}`);
-        return new oldEventSource(url);
-    };
-
-    let oldFetch = window.fetch;
-    window.fetch = function (url) {
-        console.log(`fetch: ${url}`);
-        return oldFetch(url);
-    };
-
-    // hook form reset
-    HTMLFormElement.prototype.reset = function () {
-        console.log("cancel reset form")
-    };
-    Object.defineProperty(HTMLFormElement.prototype, "reset", unChange);
-
-    // hook time func
-    let oldSetTimeout = window.setTimeout;
-    window.setTimeout = function (time) {
-        console.log(`setInterval: ${time}`);
-        return oldSetTimeout(1.5);
-    };
-
-    let oldSetInterval = window.setInterval;
-    window.setInterval = function (time) {
-        console.log(`setInterval: ${time}`);
-        return oldSetInterval(1.5);
-    }
-
-    // inject a hidden iframe to submit form
-    let iframe = document.createElement('iframe');
-    iframe.name = "i_frame";
-    iframe.display = "none";
-    document.body.appendChild(iframe);
-
-}
-
-async function handleNav(req, page) {
-    let url = req.url();
-    //console.log(req.url());
-    let parsedUrl = func.parseURL(url, page.url());
-
-    // 前或后端跳转，且跳转url经过解析后非空(host与当前page相同)，且非初始化page请求
-    if (req.isNavigationRequest() && !req.frame().parentFrame() && parsedUrl != null && parsedUrl !== page.url() && page.url() !== "about:blank") {
-        // check 302
-        request(parsedUrl, function (error, response, body) {
-            if (!error && response.statusCode.toString().substr(0, 2) === '30') {
-                if (body != null) {
-                    cluster.queue(parsedUrl);
-                }
-                cluster.queue(response.headers().location);
-                req.respond({
-                    "status": 204,
-                });
-                return;
-            }
-        });
-
-        // check frontend location
-        cluster.queue(parsedUrl);
-
-        req.respond({
-            "status": 204,
-        });
-        return;
-    }
-
-    if (url.endsWith(".ico") || req.resourceType() === "image" || req.resourceType() === "media") {
-        let img = fs.readFileSync("./image/1.jpg");
-        req.respond({
-            "status": 200,
-            "contentType": "image/png",
-            "body": img,
-        });
-        return;
-    }
-
-    if (url.indexOf("logout") !== -1) {
-        req.respond({
-            "status": 204,
-        });
-    }
-
-    if (parsedUrl != null || page.url() === "about:blank") {
-        req.continue();
-    } else {
-        req.respond({
-            "status": 204,
-        });
-    }
 }
 
 const init = async ({page, data: url}) => {
@@ -159,7 +26,7 @@ const init = async ({page, data: url}) => {
 
     // lock the navigate
     await page.on('request', async (req) => {
-        await handleNav(req, page);
+        await func.handleNav(req, page);
     });
 
     await page.goto(url, {
@@ -196,10 +63,9 @@ const init = async ({page, data: url}) => {
         for (let node of nodes) {
             let nodeName = (await node.executionContext().evaluate(node => node.nodeName, node)).toLowerCase();
             let type = await node.executionContext().evaluate(node => node.getAttribute("type"), node);
-            let res = await func.formHandler[nodeName](node, type);
-            console.log(res);
+            await func.formHandler[nodeName](node, type);
         }
-        await formNode.executionContext().evaluate(form => form.submit(), formNode);
+        formNode.executionContext().evaluate(form => form.submit(), formNode);
     }
 
     const title = await page.title();
--- func.js
+++ func.js
@@ -1,4 +1,6 @@
 import urlparser from 'url'
+import request from "request";
+import fs from "fs";
 
 // parse the nav url
 export const parseURL = (url, pageURL) => {
@@ -75,7 +77,7 @@ export const formHandler = {
                 }
             });
         }
-        //return type;
+
         switch (type) {
             case "email":
                 await node.type("lfy@qq.com");
@@ -101,12 +103,142 @@ export const formHandler = {
                 break;
             default:
                 await node.type("' or 1#", {delay: 100});
-            //node.press("Enter");
         }
         return type;
     },
     "selector": 1,
-    "textarea": (node) => {
-        node.setAttribute("value", "' or 1#");
+    "textarea": async (node) => {
+        await node.type("' or 1#", {delay: 100});
     },
 };
+
+export const handleNav = async (req, page) => {
+    let url = req.url();
+    //console.log(req.url());
+    let parsedUrl = parseURL(url, page.url());
+
+    // 前或后端跳转，且跳转url经过解析后非空(host与当前page相同)，且非初始化page请求
+    if (req.isNavigationRequest() && !req.frame().parentFrame() && parsedUrl != null && parsedUrl !== page.url() && page.url() !== "about:blank") {
+        // check 302
+        request(parsedUrl, function (error, response, body) {
+            if (!error && response.statusCode.toString().substr(0, 2) === '30') {
+                if (body != null) {
+                    cluster.queue(parsedUrl);
+                }
+                cluster.queue(response.headers().location);
+                req.respond({
+                    "status": 204,
+                });
+                return;
+            }
+        });
+
+        // check frontend location
+        cluster.queue(parsedUrl);
+
+        req.respond({
+            "status": 204,
+        });
+        return;
+    }
+
+    if (url.endsWith(".ico") || req.resourceType() === "image" || req.resourceType() === "media") {
+        let img = fs.readFileSync("./image/1.jpg");
+        req.respond({
+            "status": 200,
+            "contentType": "image/png",
+            "body": img,
+        });
+        return;
+    }
+
+    if (url.indexOf("logout") !== -1) {
+        req.respond({
+            "status": 204,
+        });
+    }
+
+    if (parsedUrl != null || page.url() === "about:blank") {
+        req.continue();
+    } else {
+        req.respond({
+            "status": 204,
+        });
+    }
+};
+
+export const hookJS = () => {
+    const unChange = {writable: false, configurable: false};
+
+    // hook history route
+    window.history.pushState = function (a, b, url) {
+        console.log(url);
+    };
+    Object.defineProperty(window.history, "pushState", unChange);
+
+    window.history.replaceState = function (a, b, url) {
+        console.log(url);
+    };
+    Object.defineProperty(window.history, "replaceState", unChange);
+
+    // hook hash change route
+    window.addEventListener("hashchange", function () {
+        console.log(document.location.href);
+    });
+
+    // hook new window
+    window.open = function (url) {
+        console.log(url);
+    };
+    Object.defineProperty(window, 'open', unChange);
+
+    window.close = function () {
+        console.log(`trying to close page.`);
+    };
+    Object.defineProperty(window, "close", unChange);
+
+    // hook new requests
+    let oldWebSocket = window.WebSocket;
+    window.WebSocket = function (url) {
+        console.log(`WebSocket: ${url}`);
+        return new oldWebSocket(url);
+    };
+
+    let oldEventSource = window.EventSource;
+    window.EventSource = function (url) {
+        console.log(`EventSource: ${url}`);
+        return new oldEventSource(url);
+    };
+
+    let oldFetch = window.fetch;
+    window.fetch = function (url) {
+        console.log(`fetch: ${url}`);
+        return oldFetch(url);
+    };
+
+    // hook form reset
+    HTMLFormElement.prototype.reset = function () {
+        console.log("cancel reset form")
+    };
+    Object.defineProperty(HTMLFormElement.prototype, "reset", unChange);
+
+    // hook time func
+    let oldSetTimeout = window.setTimeout;
+    window.setTimeout = function (time) {
+        console.log(`setInterval: ${time}`);
+        return oldSetTimeout(1.5);
+    };
+
+    let oldSetInterval = window.setInterval;
+    window.setInterval = function (time) {
+        console.log(`setInterval: ${time}`);
+        return oldSetInterval(1.5);
+    }
+
+    // inject a hidden iframe to submit form
+    let iframe = document.createElement('iframe');
+    iframe.name = "i_frame";
+    iframe.display = "none";
+    document.body.appendChild(iframe);
+
+}
